a234a5df4e1b83e7c2c39a9fd8fdb889354cc754
arnoldojqf <arnoldojqf@gmail.com>
2022-07-05 19:48:18 +0100

    Logistics Report

--- app/containers/Reports/DataTable.js
+++ app/containers/Reports/DataTable.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import MUIDataTable from 'mui-datatables';
+import MUIDataTable, { ExpandButton } from 'mui-datatables';
 import { reportsService } from '../../services/report.service';
+
+import { Table, TableBody, TableCell, TableHead, TableRow } from '@material-ui/core';
 /*
   It uses npm mui-datatables. It's easy to use, you just describe columns and data collection.
   Checkout full documentation here :
@@ -164,14 +166,45 @@ class DataTable extends React.Component {
       enableNestedDataAccess: '.', // allows nested data separated by "." (see column names and the data structure above)
       print: true,
       rowsPerPage: 10,
-      page: 1,      
+      page: 1,    
+      expandableRows: true,
+      expandableRowsHeader: false,
+      expandableRowsOnClick: true,
+      isRowExpandable: (dataIndex, expandedRows) => {
+        if (dataIndex === 3 || dataIndex === 4) return false;
+
+        // Prevent expand/collapse of any row if there are 4 rows expanded already (but allow those already expanded to be collapsed)
+        if (expandedRows.data.length > 4 && expandedRows.data.filter(d => d.dataIndex === dataIndex).length === 0)
+          return false;
+        return true;
+      },
+      rowsExpanded: [0, 1],
+      renderExpandableRow: (rowData, rowMeta) => {
+        const colSpan = rowData.length + 1;
+        return (
+          <TableRow>
+            <TableCell colSpan={colSpan}>Custom expandable row option. Data: {JSON.stringify(rowData)}</TableCell>
+          </TableRow>
+        );
+      },
+      onRowExpansionChange: (curExpanded, allExpanded, rowsExpanded) =>
+        console.log(curExpanded, allExpanded, rowsExpanded),  
     };
+
+    const components = {
+      ExpandButton: function(props) {
+        if (props.dataIndex === 3 || props.dataIndex === 4) return <div style={{ width: '24px' }} />;
+        return <ExpandButton {...props} />;
+      },
+    };
+
     return (
       <MUIDataTable
         title="Logistics"
         data={data}
         columns={columns}
         options={options}
+        components={components}
       />
     );
   }
